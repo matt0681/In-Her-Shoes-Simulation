33986c55439ecdbd2b1b2e676745281f1d583256
Matthew Lad <37879186+matt0681@users.noreply.github.com>
2021-11-29 19:04:35 -0600

    Added button scripts and fixed some of the cards.

--- In Her Shoes/Assets/Scripts/StartButton.cs
+++ In Her Shoes/Assets/Scripts/StartButton.cs
@@ -5,8 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour {
     
-    public void goToFirstScene() {
+    public void goToHouseScene() {
         SceneManager.LoadScene("Card_1_House");
+    }
+
+    public void goToBedroomScene() {
+        SceneManager.LoadScene("Bedroom");
+    }
+
+    public void goToInterrogationScene() {
+        SceneManager.LoadScene("Interogation room");
+    }
+
+    public void goToMainMenuScene() {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void goToOperatingScene() {
+        SceneManager.LoadScene("Operating Room");
     }
 
 }
